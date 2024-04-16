85db7d3a1b340d3f1820e228ae8f05921e73f2b2
Salvador Montiel <salvadormontiel8@gmail.com>
2024-04-15 22:28:46 -0600

    New setup

---
+++ client/src/main/java/com/sm/Client.java
@@ -0,0 +1,10 @@
+package com.sm;
+
+public class Client {
+  
+  public static String GREETING = "Hello World";
+
+  public static String getGreeting(String name) {
+    return "Hello " + name;
+  }
+}
---
+++ server/src/main/java/com/smontiel/Server.java
@@ -0,0 +1,11 @@
+package com.smontiel;
+
+import com.sm.Client;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println(Client.getGreeting("mundo"));
+    }
+}
